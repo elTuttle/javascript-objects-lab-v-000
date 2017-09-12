89456f84b42374d620f988b9687a60171ad0a565
John Tuttle <thejwt@gmail.com>
2017-09-12 18:37:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var newObj = recipes
+  var newObj = new Object()
+  newObj = recipes
   newObj[key] = value
   return newObj
 
